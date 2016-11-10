73783a0969739280b4aaf3c3269415d439e70f53
javierP17 <j.palaciosmartinez.jp@gmail.com>
2016-11-10 17:30:36 +0100

    projecte alumnat ARRAYLIST

--- alumnat/src/cat/iesjoaquimmir/alumnat/views/console/Application.java
+++ alumnat/src/cat/iesjoaquimmir/alumnat/views/console/Application.java
@@ -1,6 +1,9 @@
 
 package cat.iesjoaquimmir.alumnat.views.console;
 import cat.iesjoaquimmir.alumnat.views.model.businesslayer.entities.Alumne;
+import cat.iesjoaquimmir.alumnat.views.model.businesslayer.entities.Domicili;
+import cat.iesjoaquimmir.alumnat.views.model.businesslayer.entities.Modul;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
@@ -8,7 +11,7 @@ public class Application {
         int opc;
         Scanner inici = new Scanner(System.in);
         do{
-            System.out.println("\nEscull una opció:\n1.Edat-Nom-Dni\n2.dni\n3.Edat-Nom\n4.Dni-Edat\n5.Nom-Dni\n6.Surt\n");
+            System.out.println("\nEscull una opció:\n1.Edat-Nom-Dni\n2.dni\n3.Edat-Nom\n4.Dni-Edat\n5.Nom-Dni\n6.Pt8a\n7.Surt\n");
             opc = inici.nextInt();
             switch (opc){
                 case 1:
@@ -26,8 +29,11 @@ public class Application {
                 case 5:
                     e();
                     break;
+                case 6:
+                    f();
+                    break;
             } 
-          }while(opc!=6);
+          }while(opc!=7);
     }
      private static void a(){
                 //variables
@@ -108,5 +114,103 @@ public class Application {
                     Alumne opcA = new Alumne(Nom,Dni);
                 //imprimeix el resultat:
                 System.out.printf(opcA.getSalutacio());
-      }      
+      }   
+      private static void f(){
+               //Generem l'ArrayList
+               String carrer;
+               int num;
+               String pis;
+               String cp;
+               String poblacio;
+               String provincia;
+               String nom;
+               String c1;
+               String c2;
+               String dni;
+               int edat;
+               int telefonQ;
+               String nomM;
+               String descM;
+               String horesM;
+                //Array de telèfons
+                ArrayList<String> telefon = new ArrayList<String>();  
+                //Array de mòduls
+                ArrayList<Modul> modul = new ArrayList<Modul>();
+                //mètode que necesitem per operar amb la pregunta:
+                Scanner pregunta = new Scanner(System.in);
+                //pregunta el nom i el dni de l'alumne:
+                    System.out.printf("Introdueix el nom ");
+                    nom = pregunta.next();
+                    System.out.printf("Introdueix el primer cognom ");
+                    c1 = pregunta.next();
+                    System.out.printf("Introdueix el segon cognom ");
+                    c2 = pregunta.next();
+                    System.out.printf("Introdueix la edat ");
+                    edat = pregunta.nextInt();
+                    System.out.printf("Introdueix el dni ");
+                    dni = pregunta.next();
+                    System.out.printf("Introdueix la provincia ");
+                    provincia = pregunta.next();
+                    System.out.printf("Introdueix la població ");
+                    poblacio = pregunta.next();
+                    System.out.printf("Introdueix el codi postal ");
+                    cp = pregunta.next();
+                    System.out.printf("Introdueix el carrer ");
+                    carrer = pregunta.next();
+                    System.out.printf("Introdueix la número ");
+                    num = pregunta.nextInt();
+                    System.out.printf("Introdueix el pis ");
+                    pis = pregunta.next();
+                    System.out.printf("Quants telèfons vols introduïr? ");
+                    telefonQ = pregunta.nextInt();
+                    for(int o=0;o<telefonQ;o++){
+                    System.out.printf("Introdueix el telefon ");
+                    telefon.add(pregunta.next());
+                    }
+                    //System.out.printf("Introdueix un segon telefon ");
+                    //telefon.add(pregunta.next());
+                    
+                    //Introducció de mòduls a l'array
+                    System.out.printf("Introdueix el nom del mòdul ");
+                    nomM = pregunta.next();
+                    System.out.printf("Introdueix la descripció del mòdul ");
+                    descM = pregunta.next();
+                    System.out.printf("Introdueix la durada en hores del mòdul ");
+                    horesM = pregunta.next();                  
+                    Modul modul1 = new Modul(nomM,descM,horesM);
+                    modul.add(modul1);
+                    
+                    System.out.printf("Introdueix el nom del mòdul 2 ");
+                    nomM = pregunta.next();
+                    System.out.printf("Introdueix la descripció del mòdul 2 ");
+                    descM = pregunta.next();
+                    System.out.printf("Introdueix la durada en hores del mòdul 2 ");
+                    horesM = pregunta.next();                  
+                    Modul modul2 = new Modul(nomM,descM,horesM);
+                    modul.add(modul2);
+                    
+                
+                Domicili domicili = new Domicili(carrer, num, pis, cp, poblacio, provincia);
+                Alumne alumne = new Alumne( edat,nom,dni, c1,c2 ,telefon,domicili,modul);
+                //mostra el format cognom1 cognom2, nom
+                System.out.println(alumne.getNomComplert());
+                //Pinta el mateix que la linia anterior
+                System.out.println(alumne.getDomicili().getDomiciliAmigable());
+                //Industria 188, 2o 1a, 08025, Badalona (Barcelona)
+                System.out.println(domicili.getDomiciliAmigable());
+                System.out.println(alumne.getMajorMenor());
+                
+                //imprimeix els telefons de l'alumne
+                for (int i=0; i<alumne.getTelefon().size();i++){
+                   System.out.printf("Telèfon %d: %s\n",i,alumne.getTelefon().get(i));
+                }
+                
+                //imprimeix els mòduls de l'alumne
+                for (int i=0; i<alumne.getModul().size();i++){
+                   System.out.printf("Mòdul %d:\n Nom del mòdul: %s\n Descripció del mòdul: %s\n hores del mòdul: %s\n",i,
+                   alumne.getModul().get(i).getNom(), 
+                   alumne.getModul().get(i).getDescripcio(), 
+                   alumne.getModul().get(i).getDurada());
+                }
+      } 
 }
--- alumnat/src/cat/iesjoaquimmir/alumnat/views/model/businesslayer/entities/Alumne.java
+++ alumnat/src/cat/iesjoaquimmir/alumnat/views/model/businesslayer/entities/Alumne.java
@@ -1,4 +1,7 @@
 package cat.iesjoaquimmir.alumnat.views.model.businesslayer.entities;
+import cat.iesjoaquimmir.alumnat.views.model.businesslayer.entities.Domicili;
+import cat.iesjoaquimmir.alumnat.views.model.businesslayer.entities.Modul;
+import java.util.ArrayList;
 
 
 public class Alumne {
@@ -6,6 +9,12 @@ public class Alumne {
         private int edat;
         private String nom;
         private String dni;
+        private String cognom1;
+        private String cognom2;
+        //private String telefon;
+        private ArrayList<String> telefon;
+        private ArrayList<Modul> modul;
+        private Domicili domicili;
         public static final int EDAT=99;
         public static final String DNI="00000000A";
         public static final String NOM="Anonymous";
@@ -49,12 +58,79 @@ public class Alumne {
                 }
             this.dni = dni;
         }
+        
+        public String getCognom1() {
+            return cognom1;
+        }
+
+
+        public void setCognom1(String cognom1) {
+            this.cognom1 = cognom1;
+        }
+        public String getCognom2() {
+            return cognom2;
+        }
+
+
+        public void setCognom2(String cognom2) {
+            this.cognom2 = cognom2;
+        }
+        /*public String getTelefon() {
+            return telefon;
+        }
+
+
+        public void setTelefon(String telefon) {
+                if (telefon== null){
+                    throw new IllegalArgumentException(
+                        String.format("El Telefon no pot ser nul", telefon )); 
+                }
+            this.telefon = telefon;
+        }*/
+        public ArrayList<String> getTelefon() {
+            return telefon;
+        }
+        public void setTelefon(ArrayList<String> telefon) {
+            this.telefon = telefon;
+        }
+         public Domicili getDomicili() {
+            return domicili;
+        }
+
+
+        public void setDomicili(Domicili domicili) {
+            this.domicili = domicili;
+        }
+        //Getters i setters de l'array de moduls de tipus de la classe mòdul
+        public ArrayList<Modul> getModul() {
+           return modul;
+        }
+
+        public void setModul(ArrayList<Modul> modul) {
+           this.modul = modul;
+        }
+        
     //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="constructores">
-        public Alumne(int edat, String nom, String dni){
+         public Alumne(int edat, String nom, String dni, String cognom1, String cognom2, ArrayList<String> telefon, Domicili domicili,ArrayList<Modul> modul){
                 this.setEdat(edat);
                 this.setNom(nom);
                 this.setDni(dni);
+                this.setCognom1(cognom1);
+                this.setCognom2(cognom2);
+                this.setTelefon(telefon);
+                this.setDomicili(domicili);
+                this.setModul(modul);
+                
+            }
+         public Alumne(int edat, String nom, String dni, String cognom1, String cognom2, ArrayList<String> telefon){
+                this(edat,nom,dni,cognom1,cognom2,telefon,null,null);          
+            }
+        public Alumne(int edat, String nom, String dni){
+                /*this.setEdat(edat);
+                this.setNom(nom);
+                this.setDni(dni);*/
+                this(edat,nom,dni,null,null,null);
             }
             //Sobrecaàrrega del constructor
             public Alumne(String dni){
@@ -75,7 +151,31 @@ public class Alumne {
                 return String.format("Hola sóc en/la %s amb identificació %s i tinc %d anys",
                 getNom(), getDni(), getEdat());
             }
+            public String getNomComplert(){
+                return String.format("Nom complert de l'alumne: %s %s %s ",
+                getCognom2(), getCognom1(), getNom());
+            }
+            public String getMajorMenor(){
+                if(getEdat()<18){
+                return String.format("L'alumne %s amb edat %s és menor d'edat",getNom(),
+                getEdat());
+                }else{
+                return String.format("L'alumne %s amb edat %s és major d'edat",getNom(),
+                getEdat());  
+                }
+            }
+            
+            /*//mètode Array de telefons
+            public String getArray(){
+                for (int i=0; i<getTelefon().size();i++){
+                   return String.format("%s",getTelefon().get(i));
+                }
+            }*/
     //</editor-fold>
    //</editor-fold>
+
+ 
+
+
     
 }
